8884e563f3efc295917243ac2fe1064ca3eab27c
ssuhacho <ssuhacho@gmail.com>
2022-07-11 12:39:53 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
 
 const container = document.getElementById('root');
 // Create a root.
@@ -13,9 +11,9 @@ const root = ReactDOMClient.createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      
         <App />
-      </Provider>
+
     </BrowserRouter>
   </React.StrictMode>
 );
