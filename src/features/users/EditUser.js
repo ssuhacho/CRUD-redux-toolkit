2b411291247c54bdd8d2ef03e58846fb9efe00e0
ssuhacho <ssuhacho@gmail.com>
2022-07-12 14:00:33 -0500

    [Update] Redux editUser

--- src/features/users/EditUser.js
+++ src/features/users/EditUser.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import TextField from "../../components/TextField";
 import Button from "../../components/Button";
+import { editUser } from "./userSlice";
 
 const EditUser = () => {
+    const params = useParams();
+    const dispatch = useDispatch();
+    const users = useSelector(store => store.users);
     const navigate = useNavigate();
+
+    const existingUser = users.filter(user => user.id === params.id);
+    const {name, email} = existingUser[0];
     const [values, setValues] = useState({
-        name: '',
-        email: ''
+        name: name,
+        email: email
     });
 
     const handleEditUser = () =>{
         setValues({name:'', email:''});
-        console.log(values);
+        dispatch(editUser({
+            id: params.id,
+            name: values.name,
+            email: values.email
+        }))
         navigate('/');
     }
 
--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -11,9 +11,17 @@ const initialState = [
       reducers:{
           addUser: (state, action) => {
               state.push(action.payload);
+          },
+          editUser: (state, action) => {
+              const { id, name, email } = action.payload;
+              const existingUser = state.find(user => user.id === id);
+              if (existingUser) {
+                  existingUser.name = name;
+                  existingUser.email = email;
+              }
           }
       }
   });
 
-export const {addUser} = userSlice.actions;
+export const {addUser, editUser} = userSlice.actions;
 export default userSlice.reducer;
